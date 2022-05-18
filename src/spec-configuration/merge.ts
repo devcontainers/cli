d3d5e197a0afd74b684ae7fc752eb8b64d8e43b5
George <137224+gfmatthews@users.noreply.github.com>
2022-05-18 17:16:29 +0000

    Updates and moves merge to spec config

--- src/spec-configuration/merge.ts
+++ src/spec-configuration/merge.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { buildExtendBehaviorTable, ExtendBehavior } from '../spec-configuration/configuration';
+import { buildExtendBehaviorTable, ExtendBehavior } from './configuration';
 
 export async function ApplyMergeStrategyToObjects(key: string, parentValue: object, childValue: object, strategy: ExtendBehavior): Promise<Object>
 {
