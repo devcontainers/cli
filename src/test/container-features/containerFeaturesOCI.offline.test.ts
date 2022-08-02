c238d8b5418c480ba41cc66591fe4997d97924ff
Josh Spicer <josh@joshspicer.com>
2022-08-02 15:28:42 +0000

    uncomment tests

--- src/test/container-features/containerFeaturesOCI.offline.test.ts
+++ src/test/container-features/containerFeaturesOCI.offline.test.ts
@@ -1,54 +1,54 @@
 // import { assert } from 'chai';
 // import { getFeatureManifest, getFeatureBlob, getFeatureRef, createManifest } from '../../spec-configuration/containerFeaturesOCI';
 import { assert } from 'chai';
-import { calculateContentDigest, calculateTgzLayer, validateOCIFeature } from '../../spec-configuration/containerFeaturesOCI';
+import { calculateContentDigest, calculateTgzLayer, getFeatureBlob, getFeatureManifest, getFeatureRef, validateOCIFeature } from '../../spec-configuration/containerFeaturesOCI';
 import { createPlainLog, LogLevel, makeLog } from '../../spec-utils/log';
 
 export const output = makeLog(createPlainLog(text => process.stdout.write(text), () => LogLevel.Trace));
 const testAssetsDir = `${__dirname}/assets`;
 
-// describe('Test OCI Pull', () => {
+describe('Test OCI Pull', () => {
 
-//     it('Parse OCI identifier', async () => {
-//         const feat = getFeatureRef(output, 'ghcr.io/codspace/features/ruby:1');
-//         output.write(`feat: ${JSON.stringify(feat)}`);
+    it('Parse OCI identifier', async () => {
+        const feat = getFeatureRef(output, 'ghcr.io/codspace/features/ruby:1');
+        output.write(`feat: ${JSON.stringify(feat)}`);
 
-//         assert.equal(feat.id, 'ghcr.io/codspace/features/ruby');
-//         assert.equal(feat.featureName, 'ruby');
-//         assert.equal(feat.owner, 'codspace');
-//         assert.equal(feat.namespace, 'codspace/features');
-//         assert.equal(feat.registry, 'ghcr.io');
-//         assert.equal(feat.version, '1');
-//     });
+        assert.equal(feat.id, 'ghcr.io/codspace/features/ruby');
+        assert.equal(feat.featureName, 'ruby');
+        assert.equal(feat.owner, 'codspace');
+        assert.equal(feat.namespace, 'codspace/features');
+        assert.equal(feat.registry, 'ghcr.io');
+        assert.equal(feat.version, '1');
+    });
 
-//     it('Get a manifest.', async () => {
-//         const featureRef = getFeatureRef(output, 'ghcr.io/codspace/features/ruby:1');
-//         const manifest = await getFeatureManifest(output, process.env, 'https://ghcr.io/v2/codspace/features/ruby/manifests/1', featureRef);
-//         assert.isNotNull(manifest);
-//         assert.exists(manifest);
+    it('Get a manifest.', async () => {
+        const featureRef = getFeatureRef(output, 'ghcr.io/codspace/features/ruby:1');
+        const manifest = await getFeatureManifest(output, process.env, 'https://ghcr.io/v2/codspace/features/ruby/manifests/1', featureRef);
+        assert.isNotNull(manifest);
+        assert.exists(manifest);
 
-//         if (!manifest) {
-//             return;
-//         }
+        if (!manifest) {
+            return;
+        }
 
-//         output.write(`mediaType: ${manifest.mediaType}`);
-//         manifest.layers.forEach(layer => {
-//             output.write(`Layer mediaType: ${layer.mediaType}`);
-//             output.write(`Layer digest: ${layer.digest}`);
-//             output.write(`Layer size: ${layer.size}`);
+        output.write(`mediaType: ${manifest.mediaType}`);
+        manifest.layers.forEach(layer => {
+            output.write(`Layer mediaType: ${layer.mediaType}`);
+            output.write(`Layer digest: ${layer.digest}`);
+            output.write(`Layer size: ${layer.size}`);
 
-//             output.write(`Layer imageTitle: ${layer.annotations['org.opencontainers.image.title']}`);
-//         });
+            output.write(`Layer imageTitle: ${layer.annotations['org.opencontainers.image.title']}`);
+        });
 
-//         assert.equal(manifest.layers[0].digest, 'sha256:c33008d0dc12d0e631734082401bec692da809eae2ac51e24f58c1cac68fc0c9');
-//     });
+        assert.equal(manifest.layers[0].digest, 'sha256:c33008d0dc12d0e631734082401bec692da809eae2ac51e24f58c1cac68fc0c9');
+    });
 
-//     it('Download a feature', async () => {
-//         const featureRef = getFeatureRef(output, 'ghcr.io/codspace/features/ruby:1');
-//         const result = await getFeatureBlob(output, process.env, 'https://ghcr.io/v2/codspace/features/ruby/blobs/sha256:c33008d0dc12d0e631734082401bec692da809eae2ac51e24f58c1cac68fc0c9', '/tmp', '/tmp/featureTest', featureRef);
-//         assert.isTrue(result);
-//     });
-// });
+    it('Download a feature', async () => {
+        const featureRef = getFeatureRef(output, 'ghcr.io/codspace/features/ruby:1');
+        const result = await getFeatureBlob(output, process.env, 'https://ghcr.io/v2/codspace/features/ruby/blobs/sha256:c33008d0dc12d0e631734082401bec692da809eae2ac51e24f58c1cac68fc0c9', '/tmp', '/tmp/featureTest', featureRef);
+        assert.isTrue(result);
+    });
+});
 
 describe('Test Generate Manifests and Digests', () => {
     // Example: 
