c29687f2f38bae285ebd56469b6909d72554e67e
Josh Spicer <josh@joshspicer.com>
2022-08-05 13:45:39 +0000

    Convert to tabs

--- src/spec-node/featuresCLI/package.ts
+++ src/spec-node/featuresCLI/package.ts
@@ -10,89 +10,89 @@ import { getPackageConfig } from '../utils';
 import { doFeaturesPackageCommand } from './packageCommandImpl';
 
 export function featuresPackageOptions(y: Argv) {
-    return y
-        .options({
-            'feature-collection-folder': { type: 'string', alias: 'c', default: '.', description: 'Path to folder containing source code for collection of features' },
-            'output-dir': { type: 'string', alias: 'o', default: './output', description: 'Path to output directory. Will create directories as needed.' },
-            'force-clean-output-dir': { type: 'boolean', alias: 'f', default: false, description: 'Automatically delete previous output directory before packaging' },
-            'log-level': { choices: ['info' as 'info', 'debug' as 'debug', 'trace' as 'trace'], default: 'info' as 'info', description: 'Log level.' },
-        })
-        .check(argv => {
-            if (argv['scenarios'] && argv['features']) {
-                throw new Error('Cannot combine --scenarios and --features');
-            }
-            return true;
-        });
+	return y
+		.options({
+			'feature-collection-folder': { type: 'string', alias: 'c', default: '.', description: 'Path to folder containing source code for collection of features' },
+			'output-dir': { type: 'string', alias: 'o', default: './output', description: 'Path to output directory. Will create directories as needed.' },
+			'force-clean-output-dir': { type: 'boolean', alias: 'f', default: false, description: 'Automatically delete previous output directory before packaging' },
+			'log-level': { choices: ['info' as 'info', 'debug' as 'debug', 'trace' as 'trace'], default: 'info' as 'info', description: 'Log level.' },
+		})
+		.check(argv => {
+			if (argv['scenarios'] && argv['features']) {
+				throw new Error('Cannot combine --scenarios and --features');
+			}
+			return true;
+		});
 }
 
 export type FeaturesPackageArgs = UnpackArgv<ReturnType<typeof featuresPackageOptions>>;
 export interface FeaturesPackageCommandInput {
-    cliHost: CLIHost;
-    srcFolder: string;
-    outputDir: string;
-    output: Log;
-    disposables: (() => Promise<unknown> | undefined)[];
+	cliHost: CLIHost;
+	srcFolder: string;
+	outputDir: string;
+	output: Log;
+	disposables: (() => Promise<unknown> | undefined)[];
 }
 
 export function featuresPackageHandler(args: FeaturesPackageArgs) {
-    (async () => await featuresPackage(args))().catch(console.error);
+	(async () => await featuresPackage(args))().catch(console.error);
 }
 
 async function featuresPackage({
-    'feature-collection-folder': featureCollectionFolder,
-    'log-level': inputLogLevel,
-    'output-dir': outputDir,
-    'force-clean-output-dir': forceCleanOutputDir,
+	'feature-collection-folder': featureCollectionFolder,
+	'log-level': inputLogLevel,
+	'output-dir': outputDir,
+	'force-clean-output-dir': forceCleanOutputDir,
 }: FeaturesPackageArgs) {
-    const disposables: (() => Promise<unknown> | undefined)[] = [];
-    const dispose = async () => {
-        await Promise.all(disposables.map(d => d()));
-    };
+	const disposables: (() => Promise<unknown> | undefined)[] = [];
+	const dispose = async () => {
+		await Promise.all(disposables.map(d => d()));
+	};
 
-    const extensionPath = path.join(__dirname, '..', '..', '..');
-    const pkg = await getPackageConfig(extensionPath);
+	const extensionPath = path.join(__dirname, '..', '..', '..');
+	const pkg = await getPackageConfig(extensionPath);
 
-    const cwd = process.cwd();
-    const cliHost = await getCLIHost(cwd, loadNativeModule);
-    const output = createLog({
-        logLevel: mapLogLevel(inputLogLevel),
-        logFormat: 'text',
-        log: (str) => process.stdout.write(str),
-        terminalDimensions: undefined,
-    }, pkg, new Date(), disposables);
+	const cwd = process.cwd();
+	const cliHost = await getCLIHost(cwd, loadNativeModule);
+	const output = createLog({
+		logLevel: mapLogLevel(inputLogLevel),
+		logFormat: 'text',
+		log: (str) => process.stdout.write(str),
+		terminalDimensions: undefined,
+	}, pkg, new Date(), disposables);
 
-    output.write('Packaging features...', LogLevel.Info);
+	output.write('Packaging features...', LogLevel.Info);
 
-    const featuresDirResolved = cliHost.path.resolve(featureCollectionFolder);
-    if (!(await isLocalFolder(featuresDirResolved))) {
-        throw new Error(`Features folder '${featuresDirResolved}' does not exist`);
-    }
+	const featuresDirResolved = cliHost.path.resolve(featureCollectionFolder);
+	if (!(await isLocalFolder(featuresDirResolved))) {
+		throw new Error(`Features folder '${featuresDirResolved}' does not exist`);
+	}
 
-    const outputDirResolved = cliHost.path.resolve(outputDir);
-    if (await isLocalFolder(outputDirResolved)) {
-        // Output dir exists. Delete it automatically if '-f' is true
-        if (forceCleanOutputDir) {
-            await rmLocal(outputDirResolved, { recursive: true, force: true });
-        }
-        else {
-            output.write(`Output directory '${outputDirResolved}' already exists. Manually delete, or pass '-f' to continue.`, LogLevel.Warning);
-            process.exit(1);
-        }
-    }
+	const outputDirResolved = cliHost.path.resolve(outputDir);
+	if (await isLocalFolder(outputDirResolved)) {
+		// Output dir exists. Delete it automatically if '-f' is true
+		if (forceCleanOutputDir) {
+			await rmLocal(outputDirResolved, { recursive: true, force: true });
+		}
+		else {
+			output.write(`Output directory '${outputDirResolved}' already exists. Manually delete, or pass '-f' to continue.`, LogLevel.Warning);
+			process.exit(1);
+		}
+	}
 
-    // Generate output folder.
-    await mkdirpLocal(outputDirResolved);
+	// Generate output folder.
+	await mkdirpLocal(outputDirResolved);
 
-    const args: FeaturesPackageCommandInput = {
-        cliHost,
-        srcFolder: featuresDirResolved,
-        outputDir: outputDirResolved,
-        output,
-        disposables
-    };
+	const args: FeaturesPackageCommandInput = {
+		cliHost,
+		srcFolder: featuresDirResolved,
+		outputDir: outputDirResolved,
+		output,
+		disposables
+	};
 
-    const exitCode = await doFeaturesPackageCommand(args);
+	const exitCode = await doFeaturesPackageCommand(args);
 
-    await dispose();
-    process.exit(exitCode);
+	await dispose();
+	process.exit(exitCode);
 }
--- src/spec-node/featuresCLI/packageCommandImpl.ts
+++ src/spec-node/featuresCLI/packageCommandImpl.ts
@@ -6,82 +6,82 @@ import { isLocalFile, readLocalDir, readLocalFile, writeLocalFile } from '../../
 import { FeaturesPackageCommandInput } from './package';
 
 export interface SourceInformation {
-    source: string;
-    owner?: string;
-    repo?: string;
-    tag?: string;
-    ref?: string;
-    sha?: string;
+	source: string;
+	owner?: string;
+	repo?: string;
+	tag?: string;
+	ref?: string;
+	sha?: string;
 }
 export interface DevContainerCollectionMetadata {
-    sourceInformation: SourceInformation;
-    features: Feature[];
+	sourceInformation: SourceInformation;
+	features: Feature[];
 }
 
 export async function doFeaturesPackageCommand(args: FeaturesPackageCommandInput): Promise<number> {
-    const { output } = args;
+	const { output } = args;
 
-    // For each feature, package each feature and write to 'outputDir/{f}.tgz'
-    // Returns an array of feature metadata from each processed feature
-    const metadataOutput = await getFeaturesAndPackage(args);
+	// For each feature, package each feature and write to 'outputDir/{f}.tgz'
+	// Returns an array of feature metadata from each processed feature
+	const metadataOutput = await getFeaturesAndPackage(args);
 
-    if (!metadataOutput) {
-        output.write('Failed to package features', LogLevel.Error);
-        return 1;
-    }
+	if (!metadataOutput) {
+		output.write('Failed to package features', LogLevel.Error);
+		return 1;
+	}
 
-    const collection: DevContainerCollectionMetadata = {
-        sourceInformation: {
-            source: 'devcontainer-cli',
-        },
-        features: metadataOutput,
-    };
+	const collection: DevContainerCollectionMetadata = {
+		sourceInformation: {
+			source: 'devcontainer-cli',
+		},
+		features: metadataOutput,
+	};
 
-    // Write the metadata to a file
-    const metadataOutputPath = path.join(args.outputDir, 'devcontainer-collection.json');
-    await writeLocalFile(metadataOutputPath, JSON.stringify(collection, null, 4));
+	// Write the metadata to a file
+	const metadataOutputPath = path.join(args.outputDir, 'devcontainer-collection.json');
+	await writeLocalFile(metadataOutputPath, JSON.stringify(collection, null, 4));
 
-    return 0;
+	return 0;
 }
 
 async function tarDirectory(featureFolder: string, archiveName: string, outputDir: string) {
-    return new Promise<void>((resolve) => resolve(tar.create({ file: path.join(outputDir, archiveName), cwd: featureFolder }, ['.'])));
+	return new Promise<void>((resolve) => resolve(tar.create({ file: path.join(outputDir, archiveName), cwd: featureFolder }, ['.'])));
 }
 
 export async function getFeaturesAndPackage(args: FeaturesPackageCommandInput): Promise<Feature[] | undefined> {
-    const { output, srcFolder: featuresFolder, outputDir } = args;
-    const featureDirs = await readLocalDir(featuresFolder);
-    let metadatas: Feature[] = [];
+	const { output, srcFolder: featuresFolder, outputDir } = args;
+	const featureDirs = await readLocalDir(featuresFolder);
+	let metadatas: Feature[] = [];
 
-    for await (const f of featureDirs) {
-        output.write(`Processing feature: ${f}...`, LogLevel.Info);
-        if (!f.startsWith('.')) {
-            const featureFolder = path.join(featuresFolder, f);
-            const archiveName = `devcontainer-feature-${f}.tgz`;
+	for await (const f of featureDirs) {
+		output.write(`Processing feature: ${f}...`, LogLevel.Info);
+		if (!f.startsWith('.')) {
+			const featureFolder = path.join(featuresFolder, f);
+			const archiveName = `devcontainer-feature-${f}.tgz`;
 
-            // Validate minimal feature folder structure
-            const featureJsonPath = path.join(featureFolder, 'devcontainer-feature.json');
-            const installShPath = path.join(featureFolder, 'install.sh');
-            if (!isLocalFile(featureJsonPath)) {
-                output.write(`Feature '${f}' is missing a devcontainer-feature.json`, LogLevel.Error);
-                return;
-            }
-            if (!isLocalFile(installShPath)) {
-                output.write(`Feature '${f}' is missing an install.sh`, LogLevel.Error);
-                return;
-            }
+			// Validate minimal feature folder structure
+			const featureJsonPath = path.join(featureFolder, 'devcontainer-feature.json');
+			const installShPath = path.join(featureFolder, 'install.sh');
+			if (!isLocalFile(featureJsonPath)) {
+				output.write(`Feature '${f}' is missing a devcontainer-feature.json`, LogLevel.Error);
+				return;
+			}
+			if (!isLocalFile(installShPath)) {
+				output.write(`Feature '${f}' is missing an install.sh`, LogLevel.Error);
+				return;
+			}
 
-            await tarDirectory(featureFolder, archiveName, outputDir);
+			await tarDirectory(featureFolder, archiveName, outputDir);
 
-            const featureMetadata: Feature = JSON.parse(await readLocalFile(featureJsonPath, 'utf-8'));
-            metadatas.push(featureMetadata);
-        }
-    }
+			const featureMetadata: Feature = JSON.parse(await readLocalFile(featureJsonPath, 'utf-8'));
+			metadatas.push(featureMetadata);
+		}
+	}
 
-    if (metadatas.length === 0) {
-        return;
-    }
+	if (metadatas.length === 0) {
+		return;
+	}
 
-    output.write(`Packaged ${metadatas.length.toString()} features!`, LogLevel.Info);
-    return metadatas;
+	output.write(`Packaged ${metadatas.length.toString()} features!`, LogLevel.Info);
+	return metadatas;
 }
