5e68e9cba3ff84a1b8e49b25076d8b05b8be56ab
Josh Spicer <josh@joshspicer.com>
2022-08-26 10:13:04 -0400

    update annotation to align with existing annotations (#141)

--- src/spec-configuration/containerFeaturesOCIPush.ts
+++ src/spec-configuration/containerFeaturesOCIPush.ts
@@ -243,7 +243,7 @@ export async function generateCompleteManifestForIndividualFeature(output: Log, 
 	// in the manifest, in this case for UI presentation.
 	if (ociFeatureRef.registry === 'ghcr.io') {
 		annotations = {
-			'com.github.package.type': 'devcontainer-feature',
+			'com.github.package.type': 'devcontainer_feature',
 		};
 	}
 	return await calculateManifestAndContentDigest(output, tgzLayer, annotations);
@@ -263,7 +263,7 @@ export async function generateCompleteManifestForCollectionFile(output: Log, pat
 	// in the manifest, in this case for UI presentation.
 	if (collectionRef.registry === 'ghcr.io') {
 		annotations = {
-			'com.github.package.type': 'devcontainer-collection',
+			'com.github.package.type': 'devcontainer_collection',
 		};
 	}
 	return await calculateManifestAndContentDigest(output, collectionMetadataLayer, annotations);
