bec7a0f1775ba30c7bcc4af5ba9ce5478beface3
Christof Marti <chrmarti@microsoft.com>
2022-08-31 08:24:47 +0200

    Split up exec tests

---
+++ src/test/cli.exec.nonBuildKit.test.ts
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { describeTests } from './cli.exec.base';
+import { nonBuildKitOption } from './testUtils';
+
+describeTests(nonBuildKitOption);
