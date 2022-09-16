2c7e2c4d8ef51c1265ff6204e2fc321524762623
John Keech <jokeech@microsoft.com>
2022-09-16 22:53:07 +0000

    Fix e2e test to work in Codespaces

--- src/test/container-features/e2e.test.ts
+++ src/test/container-features/e2e.test.ts
@@ -33,7 +33,9 @@ describe('Dev Container Features E2E (remote)', function () {
                 assert.equal(error.error.code, 1, 'Should fail with exit code 1');
                 const res = JSON.parse(error.stdout);
                 assert.equal(res.outcome, 'error');
-                assert.ok(res.message.indexOf('Failed to fetch tarball') > -1, `Actual error msg:  ${res.message}`);
+                // "Failed to fetch tarball" happens if the test is executed without a $GITHUB_TOKEN
+                // "HTTP 404: Not Found" happens if the test is executed with a $GITHUB_TOKEN
+                assert.ok(res.message.indexOf('Failed to fetch tarball') > -1 || res.message.indexOf('HTTP 404: Not Found') > -1, `Actual error msg:  ${res.message}`);
             }
             assert.equal(success, false, 'expect non-successful call');
         });
