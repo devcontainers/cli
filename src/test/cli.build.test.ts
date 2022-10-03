6f83e40075f75d4450a71e9367d51c6010f5efec
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-03 10:18:41 -0400

    Switch to removing buildx driver after test

--- src/test/cli.build.test.ts
+++ src/test/cli.build.test.ts
@@ -187,11 +187,13 @@ describe('Dev Containers CLI', function () {
 		});
 
 		it('file /tmp/output.tar should exist when using --output type=oci,dest=/tmp/output.tar', async () => {
-			const testFolder = `${__dirname}/configs/dockerfile-with-target`;
+			const testFolder = `${__dirname}/configs/dockerfile-with-target`; 
 			const outputPath = `/tmp/output.tar`;
-			const buildxCreateRes = await shellExec(`docker buildx create --use'`);
+			await shellExec(`docker buildx create --name ocitest'`);
+			await shellExec(`docker buildx use ocitest'`);
 			const res = await shellExec(`${cli} build --workspace-folder ${testFolder} --output 'type=oci,dest=${outputPath}'`);
-			const buildxResetRes = await shellExec(`docker buildx use default'`);
+			await shellExec(`docker buildx use default'`);
+			await shellExec(`docker buildx rm ocitest'`);
 			const response = JSON.parse(res.stdout);
 			assert.equal(response.outcome, 'success');
 			assert.equal(fs.existsSync(outputPath), true);
