afb36fcf034abe2db48c145607c9c469ea5ee259
Josh Spicer <josh@joshspicer.com>
2022-10-12 21:53:48 +0000

    expose files fetched to caller of fetchTemplate

--- src/spec-configuration/containerTemplatesOCI.ts
+++ src/spec-configuration/containerTemplatesOCI.ts
@@ -3,17 +3,17 @@ import * as os from 'os';
 import { fetchOCIManifestIfExists, getBlob, getRef, OCIManifest } from './containerCollectionsOCI';
 
 
-export async function fetchTemplate(output: Log, identifier: string, templateCachePath: string) {
+export async function fetchTemplate(output: Log, identifier: string, templateCachePath: string): Promise<string[] | undefined> {
 	const templateRef = getRef(output, identifier);
 	if (!templateRef) {
 		output.write(`Failed to parse template ref for ${identifier}`, LogLevel.Error);
-		return false;
+		return;
 	}
 
 	const ociManifest = await fetchOCITemplateManifestIfExistsFromUserIdentifier(output, process.env, identifier);
 	if (!ociManifest) {
 		output.write(`Failed to fetch template manifest for ${identifier}`, LogLevel.Error);
-		return false;
+		return;
 	}
 
 	const blobUrl = `https://${templateRef.registry}/v2/${templateRef.path}/blobs/${ociManifest?.layers[0].digest}`;
@@ -26,7 +26,7 @@ export async function fetchTemplate(output: Log, identifier: string, templateCac
 		throw new Error(`Failed to download package for ${templateRef.resource}`);
 	}
 
-	return true;
+	return files;
 }
 
 
