6641069c3fe5121e7b81b735bcccc4b8ff0f767a
Josh Spicer <josh@joshspicer.com>
2022-10-13 18:44:49 +0000

    move template substitution to CLI

--- src/spec-configuration/containerTemplatesOCI.ts
+++ src/spec-configuration/containerTemplatesOCI.ts
@@ -2,18 +2,28 @@ import { Log, LogLevel } from '../spec-utils/log';
 import * as os from 'os';
 import * as path from 'path';
 import { fetchOCIManifestIfExists, getBlob, getRef, OCIManifest } from './containerCollectionsOCI';
+import { isLocalFile, readLocalFile, writeLocalFile } from '../spec-utils/pfs';
 
+export interface TemplateOptions {
+	[name: string]: string;
+}
 
-export async function fetchTemplate(output: Log, identifier: string, templateCachePath: string): Promise<string[] | undefined> {
-	const templateRef = getRef(output, identifier);
+export interface SelectedTemplate {
+	id: string;
+	options: TemplateOptions;
+}
+
+export async function fetchTemplate(output: Log, selectedTemplate: SelectedTemplate, templateDestPath: string): Promise<string[] | undefined> {
+	const { id, options } = selectedTemplate;
+	const templateRef = getRef(output, id);
 	if (!templateRef) {
-		output.write(`Failed to parse template ref for ${identifier}`, LogLevel.Error);
+		output.write(`Failed to parse template ref for ${id}`, LogLevel.Error);
 		return;
 	}
 
-	const ociManifest = await fetchOCITemplateManifestIfExistsFromUserIdentifier(output, process.env, identifier);
+	const ociManifest = await fetchOCITemplateManifestIfExistsFromUserIdentifier(output, process.env, id);
 	if (!ociManifest) {
-		output.write(`Failed to fetch template manifest for ${identifier}`, LogLevel.Error);
+		output.write(`Failed to fetch template manifest for ${id}`, LogLevel.Error);
 		return;
 	}
 
@@ -21,10 +31,21 @@ export async function fetchTemplate(output: Log, identifier: string, templateCac
 	output.write(`blob url: ${blobUrl}`, LogLevel.Trace);
 
 	const tmpDir = path.join(os.tmpdir(), 'vsch-template-temp', `${Date.now()}`);
-	const files = await getBlob(output, process.env, blobUrl, tmpDir, templateCachePath, templateRef, undefined, ['devcontainer-template.json', 'README.md', 'NOTES.md']);
+	const files = await getBlob(output, process.env, blobUrl, tmpDir, templateDestPath, templateRef, undefined, ['devcontainer-template.json', 'README.md', 'NOTES.md']);
 
 	if (!files) {
 		throw new Error(`Failed to download package for ${templateRef.resource}`);
+	}
+
+	// Scan all template files and replace any templated values.
+	for (const f of files) {
+		output.write(`Scanning file '${f}'`, LogLevel.Trace);
+		const filePath = path.join(templateDestPath, f);
+		if (await isLocalFile(filePath)) {
+			const fileContents = await readLocalFile(filePath);
+			const fileContentsReplaced = replaceTemplatedValues(output, fileContents.toString(), options);
+			await writeLocalFile(filePath, Buffer.from(fileContentsReplaced));
+		}
 	}
 
 	return files;
@@ -34,4 +55,12 @@ export async function fetchTemplate(output: Log, identifier: string, templateCac
 async function fetchOCITemplateManifestIfExistsFromUserIdentifier(output: Log, env: NodeJS.ProcessEnv, identifier: string, manifestDigest?: string, authToken?: string): Promise<OCIManifest | undefined> {
 	const templateRef = getRef(output, identifier);
 	return await fetchOCIManifestIfExists(output, env, templateRef, manifestDigest, authToken);
+}
+
+function replaceTemplatedValues(output: Log, template: string, options: TemplateOptions) {
+	const pattern = /\${templateOption:\s*(\w+?)\s*}/g; // ${templateOption:XXXX}
+	return template.replace(pattern, (_, token) => {
+		output.write(`Replacing ${token} with ${options[token]}`);
+		return options[token] || '';
+	});
 }
--- src/test/container-templates/containerTemplatesOCI.test.ts
+++ src/test/container-templates/containerTemplatesOCI.test.ts
@@ -1,22 +1,33 @@
 import { createPlainLog, LogLevel, makeLog } from '../../spec-utils/log';
 import * as assert from 'assert';
 export const output = makeLog(createPlainLog(text => process.stdout.write(text), () => LogLevel.Trace));
-import { fetchTemplate } from '../../spec-configuration/containerTemplatesOCI';
+import { fetchTemplate, SelectedTemplate } from '../../spec-configuration/containerTemplatesOCI';
 import * as path from 'path';
 import { readLocalFile } from '../../spec-utils/pfs';
 
 describe('fetchTemplate', async function () {
 
 	it('succeeds on docker-from-docker template', async () => {
+
+		// https://github.com/devcontainers/templates/tree/main/src/docker-from-docker
+		const selectedTempate: SelectedTemplate = {
+			id: 'ghcr.io/devcontainers/templates/docker-from-docker:latest',
+			options: {'installZsh': 'false', 'upgradePackages': 'true', 'dockerVersion': '20.10', 'moby': 'true', 'enableNonRootDocker': 'true' }
+		}
+
 		const dest = path.relative(process.cwd(), path.join(__dirname, 'tmp'));
-		const files = await fetchTemplate(output, 'ghcr.io/devcontainers/templates/docker-from-docker:latest', dest);
+		const files = await fetchTemplate(output, selectedTempate, dest);
 		assert.ok(files);
 		// Should only container 1 file '.devcontainer.json'.  The other 3 in this repo should be ignored.
-		// https://github.com/devcontainers/templates/tree/main/src/docker-from-docker
 		assert.strictEqual(files.length, 1);
 
 		// Read File
-		const file = await readLocalFile(path.join(dest, files[0]));
-		assert.match(file.toString(), /"name": "Docker from Docker"/);
+		const file = (await readLocalFile(path.join(dest, files[0]))).toString();
+		assert.match(file, /"name": "Docker from Docker"/);
+		assert.match(file, /"installZsh": "false"/);
+		assert.match(file, /"upgradePackages": "true"/);
+		assert.match(file, /"version": "20.10"/);
+		assert.match(file, /"moby": "true"/);
+		assert.match(file, /"enableNonRootDocker": "true"/);
 	});
 });
