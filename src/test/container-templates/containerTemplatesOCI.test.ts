c7049cede43533d65a4dac701b610022937fbdb5
Josh Spicer <josh@joshspicer.com>
2022-10-13 20:13:53 +0000

    code review and update test

--- src/test/container-templates/containerTemplatesOCI.test.ts
+++ src/test/container-templates/containerTemplatesOCI.test.ts
@@ -6,17 +6,18 @@ import * as path from 'path';
 import { readLocalFile } from '../../spec-utils/pfs';
 
 describe('fetchTemplate', async function () {
+	this.timeout('120s');
 
 	it('succeeds on docker-from-docker template', async () => {
 
 		// https://github.com/devcontainers/templates/tree/main/src/docker-from-docker
-		const selectedTempate: SelectedTemplate = {
+		const selectedTemplate: SelectedTemplate = {
 			id: 'ghcr.io/devcontainers/templates/docker-from-docker:latest',
 			options: {'installZsh': 'false', 'upgradePackages': 'true', 'dockerVersion': '20.10', 'moby': 'true', 'enableNonRootDocker': 'true' }
 		};
 
-		const dest = path.relative(process.cwd(), path.join(__dirname, 'tmp'));
-		const files = await fetchTemplate(output, selectedTempate, dest);
+		const dest = path.relative(process.cwd(), path.join(__dirname, 'tmp1'));
+		const files = await fetchTemplate(output, selectedTemplate, dest);
 		assert.ok(files);
 		// Should only container 1 file '.devcontainer.json'.  The other 3 in this repo should be ignored.
 		assert.strictEqual(files.length, 1);
@@ -34,13 +35,13 @@ describe('fetchTemplate', async function () {
 	it('succeeds on anaconda-postgres template', async () => {
 
 		// https://github.com/devcontainers/templates/tree/main/src/anaconda-postgres
-		const selectedTempate: SelectedTemplate = {
+		const selectedTemplate: SelectedTemplate = {
 			id: 'ghcr.io/devcontainers/templates/anaconda-postgres:latest',
-			options: { 'nodeVersion': '12' }
+			options: { 'nodeVersion': 'lts/*' }
 		};
 
-		const dest = path.relative(process.cwd(), path.join(__dirname, 'tmp'));
-		const files = await fetchTemplate(output, selectedTempate, dest);
+		const dest = path.relative(process.cwd(), path.join(__dirname, 'tmp2'));
+		const files = await fetchTemplate(output, selectedTemplate, dest);
 		assert.ok(files);
 		// Expected:
 		// ./environment.yml, ./.devcontainer/.env, ./.devcontainer/Dockerfile, ./.devcontainer/devcontainer.json, ./.devcontainer/docker-compose.yml, ./.devcontainer/noop.txt
@@ -48,7 +49,7 @@ describe('fetchTemplate', async function () {
 
 		// Read File
 		const file = (await readLocalFile(path.join(dest, '.devcontainer', 'Dockerfile'))).toString();
-		assert.match(file, /ARG NODE_VERSION="12"/);
+		assert.match(file, /ARG NODE_VERSION="lts\/\*"/);
 
 	});
 });
