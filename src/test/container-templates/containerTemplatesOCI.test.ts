7aa6bf679495385e49f777ad7f6b976b638d51cf
Josh Spicer <josh@joshspicer.com>
2022-10-13 23:33:40 +0000

    more tests

--- src/test/container-templates/containerTemplatesOCI.test.ts
+++ src/test/container-templates/containerTemplatesOCI.test.ts
@@ -8,7 +8,7 @@ import { readLocalFile } from '../../spec-utils/pfs';
 describe('fetchTemplate', async function () {
 	this.timeout('120s');
 
-	it('succeeds on docker-from-docker template', async () => {
+	it('succeeds on docker-from-docker template without Features', async () => {
 
 		// https://github.com/devcontainers/templates/tree/main/src/docker-from-docker
 		const selectedTemplate: SelectedTemplate = {
@@ -31,9 +31,46 @@ describe('fetchTemplate', async function () {
 		assert.match(file, /"version": "20.10"/);
 		assert.match(file, /"moby": "true"/);
 		assert.match(file, /"enableNonRootDocker": "true"/);
+
+		// Assert that the Features included in the template were not removed.
+		assert.match(file, /"ghcr.io\/devcontainers\/features\/common-utils:1": {\n/);
+		assert.match(file, /"ghcr.io\/devcontainers\/features\/docker-from-docker:1": {\n/);
 	});
 
-	it('succeeds on anaconda-postgres template', async () => {
+
+	it('succeeds on docker-from-docker template with Features', async () => {
+
+		// https://github.com/devcontainers/templates/tree/main/src/docker-from-docker
+		const selectedTemplate: SelectedTemplate = {
+			id: 'ghcr.io/devcontainers/templates/docker-from-docker:latest',
+			options: { 'installZsh': 'false', 'upgradePackages': 'true', 'dockerVersion': '20.10', 'moby': 'true', 'enableNonRootDocker': 'true' },
+			features: [{ id: 'ghcr.io/devcontainers/features/azure-cli:1', options: {} }]
+		};
+
+		const dest = path.relative(process.cwd(), path.join(__dirname, 'tmp3'));
+		const files = await fetchTemplate(output, selectedTemplate, dest);
+		assert.ok(files);
+		// Should only container 1 file '.devcontainer.json'.  The other 3 in this repo should be ignored.
+		assert.strictEqual(files.length, 1);
+
+		// Read File
+		const file = (await readLocalFile(path.join(dest, files[0]))).toString();
+		assert.match(file, /"name": "Docker from Docker"/);
+		assert.match(file, /"installZsh": "false"/);
+		assert.match(file, /"upgradePackages": "true"/);
+		assert.match(file, /"version": "20.10"/);
+		assert.match(file, /"moby": "true"/);
+		assert.match(file, /"enableNonRootDocker": "true"/);
+
+		// Assert that the Features included in the template were not removed.
+		assert.match(file, /"ghcr.io\/devcontainers\/features\/common-utils:1": {\n/);
+		assert.match(file, /"ghcr.io\/devcontainers\/features\/docker-from-docker:1": {\n/);
+
+		// Assert that our new Feature is included
+		assert.match(file, /"ghcr.io\/devcontainers\/features\/azure-cli:1": {}/);
+	});
+
+	it('succeeds on anaconda-postgres template with Features', async () => {
 
 		// https://github.com/devcontainers/templates/tree/main/src/anaconda-postgres
 		const selectedTemplate: SelectedTemplate = {
@@ -57,6 +94,5 @@ describe('fetchTemplate', async function () {
 		const devcontainer = (await readLocalFile(path.join(dest, '.devcontainer', 'devcontainer.json'))).toString();
 		assert.match(devcontainer, /"ghcr.io\/devcontainers\/features\/azure-cli:1": {}/);
 		assert.match(devcontainer, /"ghcr.io\/devcontainers\/features\/git:1": {\n\t\t\t"version": "latest",\n\t\t\t"ppa": true/);
-
 	});
 });
