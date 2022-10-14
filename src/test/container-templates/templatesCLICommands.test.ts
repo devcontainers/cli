66b76a3057c0dc3ae5967392f82070cee57296ed
Josh Spicer <josh@joshspicer.com>
2022-10-14 20:18:11 +0000

    update test

--- src/test/container-templates/templatesCLICommands.test.ts
+++ src/test/container-templates/templatesCLICommands.test.ts
@@ -43,7 +43,7 @@ describe('tests apply command', async function () {
 
 		assert.isTrue(success);
 		assert.isDefined(result);
-		assert.isTrue(result.stdout === '{"files":["./.devcontainer.json"]}', `Actual result standard out ${result.stdout}`);
+		assert.isTrue(result.stdout === '{"files":["./.devcontainer.json"]}\n');
 
 		const file = (await readLocalFile(path.join(tmp, 'template-output', '.devcontainer.json'))).toString();
 		assert.match(file, /"name": "Docker from Docker"/);
