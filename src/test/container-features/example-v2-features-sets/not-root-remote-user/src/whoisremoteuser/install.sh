debcde316616a8284d020ca03ad31f85d6c363fc
Josh Spicer <josh@joshspicer.com>
2022-11-15 23:37:29 +0000

    add new test project

---
+++ src/test/container-features/example-v2-features-sets/not-root-remote-user/src/whoisremoteuser/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'whoisremoteuser'"
+
+cat > /usr/local/bin/whoisremoteuser \
+<< EOF
+#!/bin/sh
+echo -n "$_REMOTE_USER"
+EOF
+
+chmod +x /usr/local/bin/whoisremoteuser
