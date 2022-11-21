2d19d08b00f14e846896af955e4ad11f00f193b2
Christof Marti <chrmarti@microsoft.com>
2022-11-21 14:59:07 +0100

    Keep dependencies for 3rd-party notices generator (devcontainers/cli#279)

---
+++ build/patch-packagejson.js
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+const jsonc = require('jsonc-parser');
+const fs = require('fs');
+const path = require('path');
+
+const packageJsonPath = path.join(__dirname, '..', 'package.json');
+const packageJsonText = fs.readFileSync(packageJsonPath, 'utf8');
+const packageJson = jsonc.parse(packageJsonText);
+
+const dependencies = {
+	'node-pty': packageJson.dependencies['node-pty'],
+};
+
+const edits = jsonc.modify(packageJsonText, ['dependencies'], dependencies, {});
+const patchedText = jsonc.applyEdits(packageJsonText, edits);
+fs.writeFileSync(packageJsonPath, patchedText);
