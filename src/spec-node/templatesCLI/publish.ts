71d40bd0d25e0bf1f76326a8d5d8c028f50e1db2
Josh Spicer <josh@joshspicer.com>
2022-12-16 09:34:40 -0800

    add more detail to the output of publish commands (#326)
    * add more detail to the output of publish commands
    
    * all or nothing with properties
    
    * add test asserting a republish of an existing Feature via version bump

--- src/spec-node/templatesCLI/publish.ts
+++ src/spec-node/templatesCLI/publish.ts
@@ -91,9 +91,14 @@ async function templatesPublish({
             process.exit(1);
         }
 
+        const thisResult = (publishResult?.digest && publishResult?.publishedVersions?.length > 0) ? {
+            ...publishResult,
+            version: t.version,
+        } : {};
+
         result = {
             ...result,
-            [t.id]: publishResult,
+            [t.id]: thisResult,
         };
     }
 
