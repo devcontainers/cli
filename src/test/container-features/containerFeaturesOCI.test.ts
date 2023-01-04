71d09fe84af58cadb517f1e5412ba3ab78ace50a
Josh Spicer <joshspicer@github.com>
2023-01-03 16:32:13 -0800

    Update containerFeaturesOCI.test.ts

--- src/test/container-features/containerFeaturesOCI.test.ts
+++ src/test/container-features/containerFeaturesOCI.test.ts
@@ -5,7 +5,7 @@ import { createPlainLog, LogLevel, makeLog } from '../../spec-utils/log';
 export const output = makeLog(createPlainLog(text => process.stdout.write(text), () => LogLevel.Trace));
 
 describe('getCollectionRef()', async function () {
-    this.timeout('150s');
+    this.timeout('240s');
 
 
     it('valid getCollectionRef()', async () => {
