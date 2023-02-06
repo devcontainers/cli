26406cfa4d7e746d4c3d29e27808e6151456aaff
Josh Spicer <joshspicer@github.com>
2023-02-06 21:37:01 +0000

    add test to assert installsAfter is respected

--- src/test/container-features/lifecycleHooks.test.ts
+++ src/test/container-features/lifecycleHooks.test.ts
@@ -5,6 +5,7 @@
 
 import { assert } from 'chai';
 import * as path from 'path';
+import { Feature } from '../../spec-configuration/containerFeaturesConfiguration';
 import { devContainerDown, devContainerUp, shellExec } from '../testUtils';
 
 const pkg = require('../../../package.json');
@@ -23,47 +24,105 @@ describe('Feature lifecycle hooks', function () {
 
 	describe('lifecycle-hooks-inline-commands', () => {
 
-		describe(`devcontainer up`, () => {
-			let containerId: string | null = null;
-			const testFolder = `${__dirname}/configs/lifecycle-hooks-inline-commands`;
+		let containerId: string | null = null;
+		const testFolder = `${__dirname}/configs/lifecycle-hooks-inline-commands`;
 
-			before(async () => {
-				await shellExec(`rm -f ${testFolder}/*.testMarker`, undefined, undefined, true);
-				containerId = (await devContainerUp(cli, testFolder, { 'logLevel': 'trace' })).containerId;
-			});
-
-			after(async () => {
-				await devContainerDown({ containerId });
-				await shellExec(`rm -f ${testFolder}/*.testMarker`, undefined, undefined, true);
-			});
-
-			it('marker files should exist and executed in stable order', async () => {
-				const res = await shellExec(`${cli} exec --workspace-folder ${testFolder} ls -altr`);
-				const response = JSON.parse(res.stdout);
-				assert.equal(response.outcome, 'success');
-
-				const outputOfExecCommand = res.stderr;
-				console.log(outputOfExecCommand);
-
-				assert.match(outputOfExecCommand, /0.panda.onCreateCommand.testMarker/);
-				assert.match(outputOfExecCommand, /3.panda.updateContentCommand.testMarker/);
-				assert.match(outputOfExecCommand, /6.panda.postCreateCommand.testMarker/);
-				assert.match(outputOfExecCommand, /9.panda.postStartCommand.testMarker/);
-				assert.match(outputOfExecCommand, /12.panda.postAttachCommand.testMarker/);
-
-				assert.match(outputOfExecCommand, /1.tiger.onCreateCommand.testMarker/);
-				assert.match(outputOfExecCommand, /4.tiger.updateContentCommand.testMarker/);
-				assert.match(outputOfExecCommand, /7.tiger.postCreateCommand.testMarker/);
-				assert.match(outputOfExecCommand, /10.tiger.postStartCommand.testMarker/);
-				assert.match(outputOfExecCommand, /13.tiger.postAttachCommand.testMarker/);
-
-				assert.match(outputOfExecCommand, /2.devContainer.onCreateCommand.testMarker/);
-				assert.match(outputOfExecCommand, /5.devContainer.updateContentCommand.testMarker/);
-				assert.match(outputOfExecCommand, /8.devContainer.postCreateCommand.testMarker/);
-				assert.match(outputOfExecCommand, /11.devContainer.postStartCommand.testMarker/);
-				assert.match(outputOfExecCommand, /14.devContainer.postAttachCommand.testMarker/);
-			});
+		before(async () => {
+			await shellExec(`rm -f ${testFolder}/*.testMarker`, undefined, undefined, true);
+			containerId = (await devContainerUp(cli, testFolder, { 'logLevel': 'trace' })).containerId;
 		});
+
+		after(async () => {
+			await devContainerDown({ containerId });
+			await shellExec(`rm -f ${testFolder}/*.testMarker`, undefined, undefined, true);
+		});
+
+		it('marker files should exist and executed in stable order', async () => {
+			const res = await shellExec(`${cli} exec --workspace-folder ${testFolder} ls -altr`);
+			const response = JSON.parse(res.stdout);
+			assert.equal(response.outcome, 'success');
+
+			const outputOfExecCommand = res.stderr;
+			console.log(outputOfExecCommand);
+
+			assert.match(outputOfExecCommand, /0.panda.onCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /3.panda.updateContentCommand.testMarker/);
+			assert.match(outputOfExecCommand, /6.panda.postCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /9.panda.postStartCommand.testMarker/);
+			assert.match(outputOfExecCommand, /12.panda.postAttachCommand.testMarker/);
+
+			assert.match(outputOfExecCommand, /1.tiger.onCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /4.tiger.updateContentCommand.testMarker/);
+			assert.match(outputOfExecCommand, /7.tiger.postCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /10.tiger.postStartCommand.testMarker/);
+			assert.match(outputOfExecCommand, /13.tiger.postAttachCommand.testMarker/);
+
+			assert.match(outputOfExecCommand, /2.devContainer.onCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /5.devContainer.updateContentCommand.testMarker/);
+			assert.match(outputOfExecCommand, /8.devContainer.postCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /11.devContainer.postStartCommand.testMarker/);
+			assert.match(outputOfExecCommand, /14.devContainer.postAttachCommand.testMarker/);
+		});
+	});
+
+	describe('lifecycle-hooks-alternative-order', () => {
+		// This is the same test as 'lifecycle-hooks-inline-commands' but with the the 'installsAfter' order changed (tiger -> panda -> devContainer).
+
+		let containerId: string | null = null;
+		const testFolder = `${__dirname}/configs/temp_lifecycle-hooks-alternative-order`;
+
+		before(async () => {
+			await shellExec(`rm -rf ${testFolder}`, undefined, undefined, true);
+			await shellExec(`mkdir -p ${testFolder}`);
+			await shellExec(`bash -c 'cp -r . ${testFolder}'`, { cwd: `${__dirname}/configs/lifecycle-hooks-inline-commands` });
+
+			// Read in the JSON from the two Feature's devcontainer-feature.json
+			const pandaFeatureJson: Feature = JSON.parse((await shellExec(`cat ${testFolder}/.devcontainer/panda/devcontainer-feature.json`)).stdout);
+			const tigerFeatureJson: Feature = JSON.parse((await shellExec(`cat ${testFolder}/.devcontainer/tiger/devcontainer-feature.json`)).stdout);
+
+			// Remove the installsAfter from the tiger's devcontainer-feature.json and add it to the panda's devcontainer-feature.json
+			delete tigerFeatureJson.installsAfter;
+			pandaFeatureJson.installsAfter = ['./tiger'];
+
+			// Write the JSON back to the two Feature's devcontainer-feature.json
+			await shellExec(`echo '${JSON.stringify(pandaFeatureJson)}' > ${testFolder}/.devcontainer/panda/devcontainer-feature.json`);
+			await shellExec(`echo '${JSON.stringify(tigerFeatureJson)}' > ${testFolder}/.devcontainer/tiger/devcontainer-feature.json`);
+
+			containerId = (await devContainerUp(cli, testFolder, { 'logLevel': 'trace' })).containerId;
+		});
+
+		after(async () => {
+			await devContainerDown({ containerId });
+			await shellExec(`rm -f ${testFolder}/*.testMarker`, undefined, undefined, true);
+		});
+
+		it('marker files should exist and executed in stable order', async () => {
+			const res = await shellExec(`${cli} exec --workspace-folder ${testFolder} ls -altr`);
+			const response = JSON.parse(res.stdout);
+			assert.equal(response.outcome, 'success');
+
+			const outputOfExecCommand = res.stderr;
+			console.log(outputOfExecCommand);
+
+			assert.match(outputOfExecCommand, /0.tiger.onCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /3.tiger.updateContentCommand.testMarker/);
+			assert.match(outputOfExecCommand, /6.tiger.postCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /9.tiger.postStartCommand.testMarker/);
+			assert.match(outputOfExecCommand, /12.tiger.postAttachCommand.testMarker/);
+
+			assert.match(outputOfExecCommand, /1.panda.onCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /4.panda.updateContentCommand.testMarker/);
+			assert.match(outputOfExecCommand, /7.panda.postCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /10.panda.postStartCommand.testMarker/);
+			assert.match(outputOfExecCommand, /13.panda.postAttachCommand.testMarker/);
+
+			assert.match(outputOfExecCommand, /2.devContainer.onCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /5.devContainer.updateContentCommand.testMarker/);
+			assert.match(outputOfExecCommand, /8.devContainer.postCreateCommand.testMarker/);
+			assert.match(outputOfExecCommand, /11.devContainer.postStartCommand.testMarker/);
+			assert.match(outputOfExecCommand, /14.devContainer.postAttachCommand.testMarker/);
+		});
+
 	});
 
 	describe('lifecycle-hooks-advanced', () => {
