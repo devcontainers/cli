92a0e1c7541e89036a2b500c06a371f88a6fb499
Josh Spicer <joshspicer@github.com>
2023-04-10 22:52:54 +0000

    example FeatureSet using dependsOn

---
+++ src/test/container-features/example-v2-features-sets/dependsOn/src/D/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+NAME="D"
+
+echo "Installing ${NAME}"
+MAGIC_NUMBER=${MAGICNUMBER}
+echo "The magic number is ${MAGIC_NUMBER}"
+touch ${_REMOTE_USER_HOME}/magic-number-${NAME}-${MAGIC_NUMBER}-$(date +%s)
+echo "Done installing ${NAME}"
