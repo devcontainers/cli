6717651c35845ccc6a506dfa785e713cd88d6b48
Christof Marti <chrmarti@microsoft.com>
2025-08-15 17:36:23 +0200

    debian:latest dropped adduser / addgroup

--- src/test/getEntPasswd.test.ts
+++ src/test/getEntPasswd.test.ts
@@ -17,22 +17,25 @@ describe('getEntPasswdShellCommand', function () {
 		{
 			image: 'busybox',
 			getentPath: undefined,
-			addUserOptions: '-D -h',
+			addUserCommand: 'adduser -D -h /home/foo foo\\\\bar',
 			userName: 'foo\\bar',
+			homeFolder: '/home/foo',
 		},
 		{
 			image: 'debian',
 			getentPath: '/usr/bin/getent',
-			addUserOptions: '--disabled-password --allow-all-names --gecos "" --home',
+			addUserCommand: 'useradd -m -d /home/foo --badname foo\\\\bar',
 			userName: 'foo\\bar',
+			homeFolder: '/home/foo',
 		},
 		{
 			image: 'alpine',
 			getentPath: '/usr/bin/getent',
-			addUserOptions: '-D -h',
+			addUserCommand: 'adduser -D -h /home/foo foo_bar',
 			userName: 'foo_bar', // Alpine doesn't support backslash in user names.
+			homeFolder: '/home/foo',
 		},
-	].forEach(({ image, getentPath, addUserOptions, userName }) => {
+	].forEach(({ image, getentPath, addUserCommand, userName, homeFolder }) => {
 		it(`should work with ${image} ${getentPath ? 'with' : 'without'} getent command`, async () => {
 			const res = await shellExec(`docker run -d ${image} sleep inf`);
 			const containerId = res.stdout.trim();
@@ -48,17 +51,17 @@ describe('getEntPasswdShellCommand', function () {
 				.catch(() => undefined);
 			assert.strictEqual(which?.stdout.trim(), getentPath);
 
-			await shellServer.exec(`adduser ${addUserOptions} /home/foo ${userName.replaceAll('\\', '\\\\')}`);
+			await shellServer.exec(addUserCommand);
 
 			const userByName = await getUserFromPasswdDB(shellServer, userName);
 			assert.ok(userByName);
 			assert.strictEqual(userByName.name, userName);
-			assert.strictEqual(userByName.home, '/home/foo');
+			assert.strictEqual(userByName.home, homeFolder);
 
 			const userById = await getUserFromPasswdDB(shellServer, userByName.uid);
 			assert.ok(userById);
 			assert.strictEqual(userById.name, userName);
-			assert.strictEqual(userById.home, '/home/foo');
+			assert.strictEqual(userById.home, homeFolder);
 
 			const nonexistentUser = await getUserFromPasswdDB(shellServer, '123456');
 			assert.strictEqual(undefined, nonexistentUser);
